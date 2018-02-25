be59ab505337dc38b5b8dc5a5e8e065aca696627
Margaret Berry <codeberry1@gmail.com>
2018-02-24 17:58:56 -0800

    interpolate api key as a variable

--- js/fbi-interface.js
+++ js/fbi-interface.js
@@ -1,4 +1,5 @@
 import { Fbi } from './../js/fbi.js';
+var apiKey = require('./../.env').apiKey;
  google.charts.load('current', {'packages':['corechart']});
 $(document).ready(function() {
 
@@ -8,7 +9,7 @@ $(document).ready(function() {
     let year = parseInt($('#year').val());
     let crime = $('#crimeType').val();
     debugger;
-    let promise = fbi.makePromise(`https://api.usa.gov/crime/fbi/ucr/estimates/states/OR?page=1&per_page=23&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`);
+    let promise = fbi.makePromise(`https://api.usa.gov/crime/fbi/ucr/estimates/states/OR?page=1&per_page=23&output=json&api_key=${apiKey}`);
 
     fbi.arrayOfCrime(promise,year,crime);
 
