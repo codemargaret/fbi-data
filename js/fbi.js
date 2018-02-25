6db4a681cff8c1ea12234e711b9a9d4c02468afb
Margaret Berry <codeberry1@gmail.com>
2018-02-24 17:59:21 -0800

    remove commented code and update variable names to be more specific

--- js/fbi.js
+++ js/fbi.js
@@ -3,62 +3,10 @@ export class Fbi {
 
   }
 
-  // callApi(yearToFind, crimeToFind){
-  //   $.ajax({
-  //     url: `https://api.usa.gov/crime/fbi/ucr/estimates/states/OR?page=1&per_page=10&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`,
-  //     type: 'GET',
-  //     data: {
-  //       format: 'json'
-  //     },
-  //     success: function(response) {
-  //       let thisYear;
-  //       for(let i = 0; i < 23; ++i){
-  //         thisYear = response.results[i].year;
-  //         if(thisYear === yearToFind){
-  //           $('.showYear').text(`Year: ${response.results[i].year}`);
-  //           $('.showState').text(`State:   ${response.results[i].state_abbr}.`);
-  //           $('.showPop').text(`Population:  ${response.results[i].population}.`);
-  //           if(crimeToFind === "homicide"){
-  //             $('.crime').text(`Homicides: ${response.results[i].homicide}`);
-  //           }
-  //           if(crimeToFind === "violent_crime"){
-  //             $('.crime').text(`Violent Crimes: ${response.results[i].violent_crime}`);
-  //           }
-  //           if(crimeToFind === "rape_legacy"){
-  //             $('.crime').text(`Rapes: ${response.results[i].rape_legacy}`);
-  //           }
-  //           if(crimeToFind === "robbery"){
-  //             $('.crime').text(`Robberies: ${response.results[i].robbery}`);
-  //           }
-  //           if(crimeToFind === "aggravated_assault"){
-  //             $('.crime').text(`Aggravated Assaults: ${response.results[i].aggravated_assault}`);
-  //           }
-  //           if(crimeToFind === "property_crime"){
-  //             $('.crime').text(`Property Crimes: ${response.results[i].property_crime}`);
-  //           }
-  //           if(crimeToFind === "burglary"){
-  //             $('.crime').text(`Burglaries: ${response.results[i].burglary}`);
-  //           }
-  //           if(crimeToFind === "larceny"){
-  //             $('.crime').text(`Larcenies: ${response.results[i].larceny}`);
-  //           }
-  //           if(crimeToFind === "motor_vehicle_theft"){
-  //             $('.crime').text(`Grand Theft Autos: ${response.results[i].motor_vehicle_theft}`);
-  //           }
-  //           return true;
-  //         }
-  //       }
-  //     },
-  //     error: function() {
-  //       $('#errors').text("There was an error processing your request. Please try again.");
-  //     }
-  //   });
-  // }
-
-  makePromise(api_key){
+  makePromise(endpoint){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = api_key;
+      let url = endpoint;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -154,10 +102,7 @@ export class Fbi {
         }
         crimeArray[i+1].push((body.results[i].population)/1000);
       }
-      // $('.crimearr').text(crimeArray);
-      //google.charts.load('current', {'packages':['corechart']});
-      // google.charts.setOnLoadCallback(drawChart);
-      debugger;
+
         let data = google.visualization.arrayToDataTable(crimeArray);
 
         let options = {
